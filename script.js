e37e7ee5d62e5975d93c25a5731d730b90dbcb64
rishabh <rishabhj945@gmail.com>
2021-08-16 12:15:09 +0530

    rectify

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-
 function generateRandomString(length) {
+  var result = "";
+  var characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    var result = "";
-    var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < length; i++) {
+    result =
+      result +
+      "" +
+      characters.charAt(Math.floor(Math.random() * characters.length));
+  }
 
-    for (let i = 0; i < length; i++) {
-        result = result + "" + characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-
-    return result;
-
+  return result;
 }
 
 if (!localStorage.getItem("tasks")) {
-    localStorage.setItem("tasks", JSON.stringify([]));
+  localStorage.setItem("tasks", JSON.stringify([]));
 }
 //  <=============== Nav Bar =====================>
 
@@ -22,128 +22,113 @@ let colors = document.querySelectorAll(".filter div");
 let grid = document.querySelector(".grid");
 
 let color = {
-
-    pink: "#d595aa",
-    green: "#91e6c7",
-    blue: "#5ecdde",
-    black: "black"
-}
+  pink: "#d595aa",
+  green: "#91e6c7",
+  blue: "#5ecdde",
+  black: "black",
+};
 
 let select = false;
 for (let i = 0; i < colors.length; i++) {
-    colors[i].addEventListener("click", (e) => {
+  colors[i].addEventListener("click", (e) => {
+    let clr = colors[i].classList[0];
+    for (let i = 0; i < colors.length; i++) {
+      if (grid.classList.contains(colors[i].classList[0])) {
+        grid.classList.remove(colors[i].classList[0]);
+        break;
+      }
+    }
+    grid.classList.add(clr);
 
-        let clr = colors[i].classList[0];
-              for(let i=0;i<colors.length;i++)
-              {
-                  if(grid.classList.contains(colors[i].classList[0]))
-                  {
-                      grid.classList.remove(colors[i].classList[0]);
-                      break;
-                  }
-              }
-              grid.classList.add(clr);
-
-        if (select) {
-            select = false;
-            if (colors[i].parentElement.classList.contains("Select-state")) {
-                colors[i].parentElement.classList.remove("Select-state");
-                loadTasks();
-            }
-
-        }
-        else {
-            select = true;
-            colors[i].parentElement.classList.add("Select-state");
-            loadTasks(clr);
-        }
-
-    });
+    if (select) {
+      select = false;
+      if (colors[i].parentElement.classList.contains("Select-state")) {
+        colors[i].parentElement.classList.remove("Select-state");
+        loadTasks();
+      }
+    } else {
+      select = true;
+      colors[i].parentElement.classList.add("Select-state");
+      loadTasks(clr);
+    }
+  });
 }
-
 
 //================= ticket  ====================  //
 
 function ticket(priority_color, text, id) {
-    let tkt = document.createElement("div");
-    let grid = document.querySelector(".grid");
-    tkt.classList.add("ticket");
+  let tkt = document.createElement("div");
+  let grid = document.querySelector(".grid");
+  tkt.classList.add("ticket");
 
-    tkt.innerHTML = ` <div class="color ${priority_color}"></div>
+  tkt.innerHTML = ` <div class="color ${priority_color}"></div>
      <div class="ticket-id">${"#" + id}</div>
      <div class="ticket-text" spellcheck="false" contenteditable> ${text}</div>
-    </div>`
+    </div>`;
 
-    storeinLocalStorage(priority_color, id, text);
+  storeinLocalStorage(priority_color, id, text);
 
-    let WriteArea = tkt.querySelector(".ticket-text");
+  let WriteArea = tkt.querySelector(".ticket-text");
 
-    WriteArea.addEventListener("input", (e) => {
+  WriteArea.addEventListener("input", (e) => {
+    let tktid = e.currentTarget.parentElement
+      .querySelector(".ticket-id")
+      .innerText.split("#")[1];
 
-        let tktid = e.currentTarget.parentElement.querySelector(".ticket-id").innerText.split("#")[1];
+    let text = WriteArea.innerText;
 
-        let text = WriteArea.innerText;
+    let tasksArr = JSON.parse(localStorage.getItem("tasks"));
 
-        let tasksArr = JSON.parse(localStorage.getItem("tasks"));
+    for (let i = 0; i < tasksArr.length; i++) {
+      if (tasksArr[i]["id"] == tktid) {
+        tasksArr[i]["text"] = text;
+        break;
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+  });
 
-        for (let i = 0; i < tasksArr.length; i++) {
-            if (tasksArr[i]["id"] == tktid) {
-                tasksArr[i]["text"] = text;
-                break;
-            }
-        }
-        localStorage.setItem("tasks", JSON.stringify(tasksArr));
+  let color_btn = tkt.querySelector(".color");
 
-    });
+  let colorClass = ["pink-btn2", "blue-btn2", "green-btn2", "black-btn2"];
 
-    let color_btn = tkt.querySelector(".color");
+  color_btn.addEventListener("click", (e) => {
+    let currColor = color_btn.classList[1];
+    let idx = colorClass.indexOf(currColor);
+    idx = (idx + 1) % 4;
+    color_btn.classList.remove(currColor);
 
-    let colorClass = ["pink-btn2", "blue-btn2", "green-btn2", "black-btn2"];
+    let tktid = e.currentTarget.parentElement
+      .querySelector(".ticket-id")
+      .innerText.split("#")[1];
+    let tasksArr = JSON.parse(localStorage.getItem("tasks"));
 
+    for (let i = 0; i < tasksArr.length; i++) {
+      if (tasksArr[i]["id"] == tktid) {
+        tasksArr[i].priority_color = colorClass[idx];
+        break;
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+    color_btn.classList.add(colorClass[idx]);
+  });
+  grid.appendChild(tkt);
 
+  tkt.addEventListener("click", (e) => {
+    if (deleteState) {
+      let id = tkt.querySelector(".ticket-id").innerText.split("#")[1];
 
-    color_btn.addEventListener("click", (e) => {
-        let currColor = color_btn.classList[1];
-        let idx = colorClass.indexOf(currColor);
-        idx = (idx + 1) % 4;
-        color_btn.classList.remove(currColor);
+      let tasks = JSON.parse(localStorage.getItem("tasks"));
 
+      tasks = tasks.filter(function (e) {
+        return e.id != id;
+      });
 
+      localStorage.setItem("tasks", JSON.stringify(tasks));
 
-        let tktid = e.currentTarget.parentElement.querySelector(".ticket-id").innerText.split("#")[1];
-        let tasksArr = JSON.parse(localStorage.getItem("tasks"));
-
-        for (let i = 0; i < tasksArr.length; i++) {
-            if (tasksArr[i]["id"] == tktid) {
-                tasksArr[i].priority_color = colorClass[idx];
-                break;
-            }
-        }
-        localStorage.setItem("tasks", JSON.stringify(tasksArr));
-        color_btn.classList.add(colorClass[idx]);
-
-    });
-    grid.appendChild(tkt);
-
-    tkt.addEventListener("click", (e) => {
-
-        if (deleteState) {
-            let id = tkt.querySelector(".ticket-id").innerText.split("#")[1];
-
-            let tasks = JSON.parse(localStorage.getItem("tasks"));
-
-            tasks = tasks.filter(function (e) {
-
-                return e.id != id;
-            });
-
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-
-            tkt.remove();
-        }
-
-    });
-
+      tkt.remove();
+    }
+  });
 }
 
 //================= dialog Box + addbtn ===================//
@@ -152,23 +137,19 @@ let body = document.querySelector("body");
 let addbtn = document.querySelector(".add-btn");
 
 addbtn.addEventListener("click", (e) => {
-
-    if (deleteState) {
-        deleteState = false;
-        if (deletebtn.classList.contains("delete-state")) {
-
-            deletebtn.classList.remove("delete-state");
-        }
+  if (deleteState) {
+    deleteState = false;
+    if (deletebtn.classList.contains("delete-state")) {
+      deletebtn.classList.remove("delete-state");
     }
-    if (modalVisible) return;
+  }
+  if (modalVisible) return;
 
+  let dialogbox = document.createElement("div");
 
+  dialogbox.classList.add("dialog-box");
 
-    let dialogbox = document.createElement("div");
-
-    dialogbox.classList.add("dialog-box");
-
-    dialogbox.innerHTML = `<div class="text-area" spellcheck="false" contenteditable>
+  dialogbox.innerHTML = `<div class="text-area" spellcheck="false" contenteditable>
 Enter Your Task </div>
 <div class="priority-btn">
 <div class="btns">
@@ -179,228 +160,188 @@ Enter Your Task </div>
   <div class="black-btn2 add-btn-border2"></div>
 
 </div>
-</div>`
-    body.appendChild(dialogbox);
-    modalVisible = true;
-    let clr = "black-btn2";
-    let priority_btn = document.querySelectorAll(".btns div");
+</div>`;
+  body.appendChild(dialogbox);
+  modalVisible = true;
+  let clr = "black-btn2";
+  let priority_btn = document.querySelectorAll(".btns div");
 
-    for (let i = 0; i < priority_btn.length; i++) {
-        priority_btn[i].addEventListener("click", (e) => {
+  for (let i = 0; i < priority_btn.length; i++) {
+    priority_btn[i].addEventListener("click", (e) => {
+      clr = priority_btn[i].classList[0];
 
-            clr = priority_btn[i].classList[0];
+      for (let j = 0; j < priority_btn.length; j++) {
+        priority_btn[j].classList.remove("add-btn-border");
+        priority_btn[j].classList.remove("add-btn-border2");
+      }
+      if (clr != "black-btn2") {
+        priority_btn[i].classList.add("add-btn-border");
+      } else {
+        priority_btn[i].classList.add("add-btn-border2");
+      }
+    });
+  }
 
-            for (let j = 0; j < priority_btn.length; j++) {
-
-                priority_btn[j].classList.remove("add-btn-border");
-                priority_btn[j].classList.remove("add-btn-border2");
-
-            }
-            if (clr != "black-btn2") {
-                priority_btn[i].classList.add("add-btn-border");
-            }
-            else {
-                priority_btn[i].classList.add("add-btn-border2");
-            }
-
-
-        });
+  let textarea = dialogbox.querySelector(".text-area");
+  let count = 0;
+  textarea.addEventListener("click", (e) => {
+    count++;
+    if (count == 1) {
+      textarea.textContent = "";
+    } else {
+      return;
     }
+  });
+  let shiftdown = false;
+  textarea.addEventListener("keypress", (e) => {
+    if (!shiftdown && e.key == "Enter") {
+      let text = textarea.textContent;
+      let id = generateRandomString(6);
+      ticket(clr, text, id);
+      dialogbox.remove();
+      modalVisible = false;
+    }
+  });
 
-    let textarea = dialogbox.querySelector(".text-area");
-    let count = 0;
-    textarea.addEventListener("click", (e) => {
-        count++;
-        if (count == 1) {
-            textarea.textContent = "";
-        }
-        else {
-            return;
-        }
-    });
-    let shiftdown = false;
-    textarea.addEventListener("keypress", (e) => {
-
-        if (!shiftdown && e.key == "Enter") {
-            let text = textarea.textContent;
-            let id = generateRandomString(6);
-            ticket(clr, text, id);
-            dialogbox.remove();
-            modalVisible = false;
-        }
-
-    });
-
-
-    textarea.addEventListener("keydown", (e) => {
-        if (e.key == "Shift") {
-            shiftdown = true;
-
-        }
-    });
-    textarea.addEventListener("keyup", (e) => {
-
-        if (e.key == "Shift") {
-            shiftdown = false;
-            console.log(shiftdown);
-        }
-
-    });
-
+  textarea.addEventListener("keydown", (e) => {
+    if (e.key == "Shift") {
+      shiftdown = true;
+    }
+  });
+  textarea.addEventListener("keyup", (e) => {
+    if (e.key == "Shift") {
+      shiftdown = false;
+      console.log(shiftdown);
+    }
+  });
 });
 //=======================  delete btn  ====================== ///////////////////////
 let deletebtn = document.querySelector(".delete-btn");
 
 let deleteState = false;
 deletebtn.addEventListener("click", (e) => {
-
-    if (deleteState) {
-        deleteState = false;
-        deletebtn.classList.remove("delete-state");
-    }
-    else {
-        deleteState = true;
-        deletebtn.classList.add("delete-state");
-    }
-
+  if (deleteState) {
+    deleteState = false;
+    deletebtn.classList.remove("delete-state");
+  } else {
+    deleteState = true;
+    deletebtn.classList.add("delete-state");
+  }
 });
-
 
 let sidePanelBtn = document.querySelector(".toggle-btn");
 let container = document.querySelector(".container");
 let canvas = document.querySelector(".sheet");
 sidePanelBtn.addEventListener("click", (e) => {
-    canvas.style.width = "100%";
-
+  canvas.style.width = "100%";
 });
 
 let closeBtn = document.querySelector(".close-btn");
 closeBtn.addEventListener("click", (e) => {
-
-    canvas.style.width = "0%";
-
+  canvas.style.width = "0%";
 });
 
-
-
 function storeinLocalStorage(priority_color, id, text) {
+  let taskArr = JSON.parse(localStorage.getItem("tasks"));
+  let obj = { id, priority_color, text };
 
-    let taskArr = JSON.parse(localStorage.getItem("tasks"));
-    let obj = { id, priority_color, text };
+  taskArr.push(obj);
 
-    taskArr.push(obj);
-
-    localStorage.setItem("tasks", JSON.stringify(taskArr));
-
+  localStorage.setItem("tasks", JSON.stringify(taskArr));
 }
 
 function loadtickets(priority_color, text, id) {
-    let tkt = document.createElement("div");
-    let grid = document.querySelector(".grid");
-    tkt.classList.add("ticket");
+  let tkt = document.createElement("div");
+  let grid = document.querySelector(".grid");
+  tkt.classList.add("ticket");
 
-    tkt.innerHTML = ` <div class="color ${priority_color}"></div>
+  tkt.innerHTML = ` <div class="color ${priority_color}"></div>
      <div class="ticket-id">${"#" + id}</div>
      <div class="ticket-text" spellcheck="false" contenteditable> ${text}</div>
-    </div>`
+    </div>`;
 
+  let WriteArea = tkt.querySelector(".ticket-text");
 
+  WriteArea.addEventListener("input", (e) => {
+    let tktid = e.currentTarget.parentElement
+      .querySelector(".ticket-id")
+      .innerText.split("#")[1];
 
-    let WriteArea = tkt.querySelector(".ticket-text");
+    let text = WriteArea.innerText;
 
-    WriteArea.addEventListener("input", (e) => {
+    let tasksArr = JSON.parse(localStorage.getItem("tasks"));
 
-        let tktid = e.currentTarget.parentElement.querySelector(".ticket-id").innerText.split("#")[1];
+    for (let i = 0; i < tasksArr.length; i++) {
+      if (tasksArr[i]["id"] == tktid) {
+        tasksArr[i]["text"] = text;
+        break;
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+  });
 
-        let text = WriteArea.innerText;
+  let color_btn = tkt.querySelector(".color");
 
-        let tasksArr = JSON.parse(localStorage.getItem("tasks"));
+  let colorClass = ["pink-btn2", "blue-btn2", "green-btn2", "black-btn2"];
 
-        for (let i = 0; i < tasksArr.length; i++) {
-            if (tasksArr[i]["id"] == tktid) {
-                tasksArr[i]["text"] = text;
-                break;
-            }
-        }
-        localStorage.setItem("tasks", JSON.stringify(tasksArr));
+  color_btn.addEventListener("click", (e) => {
+    let currColor = color_btn.classList[1];
+    let idx = colorClass.indexOf(currColor);
+    idx = (idx + 1) % 4;
+    color_btn.classList.remove(currColor);
 
-    });
+    let tktid = e.currentTarget.parentElement
+      .querySelector(".ticket-id")
+      .innerText.split("#")[1];
+    let tasksArr = JSON.parse(localStorage.getItem("tasks"));
 
-    let color_btn = tkt.querySelector(".color");
+    for (let i = 0; i < tasksArr.length; i++) {
+      if (tasksArr[i]["id"] == tktid) {
+        tasksArr[i].priority_color = colorClass[idx];
+        break;
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+    color_btn.classList.add(colorClass[idx]);
+  });
+  grid.appendChild(tkt);
 
-    let colorClass = ["pink-btn2", "blue-btn2", "green-btn2", "black-btn2"];
+  tkt.addEventListener("click", (e) => {
+    if (deleteState) {
+      let id = tkt.querySelector(".ticket-id").innerText.split("#")[1];
 
+      let tasks = JSON.parse(localStorage.getItem("tasks"));
 
+      tasks = tasks.filter(function (e) {
+        return e.id != id;
+      });
 
-    color_btn.addEventListener("click", (e) => {
-        let currColor = color_btn.classList[1];
-        let idx = colorClass.indexOf(currColor);
-        idx = (idx + 1) % 4;
-        color_btn.classList.remove(currColor);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
 
-
-
-        let tktid = e.currentTarget.parentElement.querySelector(".ticket-id").innerText.split("#")[1];
-        let tasksArr = JSON.parse(localStorage.getItem("tasks"));
-
-        for (let i = 0; i < tasksArr.length; i++) {
-            if (tasksArr[i]["id"] == tktid) {
-                tasksArr[i].priority_color = colorClass[idx];
-                break;
-            }
-        }
-        localStorage.setItem("tasks", JSON.stringify(tasksArr));
-        color_btn.classList.add(colorClass[idx]);
-
-    });
-    grid.appendChild(tkt);
-
-    tkt.addEventListener("click", (e) => {
-
-        if (deleteState) {
-            let id = tkt.querySelector(".ticket-id").innerText.split("#")[1];
-
-            let tasks = JSON.parse(localStorage.getItem("tasks"));
-
-            tasks = tasks.filter(function (e) {
-
-                return e.id != id;
-            });
-
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-
-            tkt.remove();
-        }
-
-    });
-
+      tkt.remove();
+    }
+  });
 }
 
 function loadTasks(colour) {
+  let allTickets = document.querySelectorAll(".ticket");
 
+  for (let i = 0; i < allTickets.length; i++) {
+    allTickets[i].remove();
+  }
 
-    let allTickets = document.querySelectorAll(".ticket");
-
-    for (let i = 0; i < allTickets.length; i++) {
-        allTickets[i].remove();
-    }
-
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    if (colour != undefined) {
-        for (let i = 0; i < tasks.length; i++) {
-            if (tasks[i].priority_color == colour)
-                loadtickets(tasks[i].priority_color, tasks[i].text, tasks[i].id);
-        }
-        return;
-    }
-
-
-
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (colour != undefined) {
     for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].priority_color == colour)
         loadtickets(tasks[i].priority_color, tasks[i].text, tasks[i].id);
     }
+    return;
+  }
+
+  for (let i = 0; i < tasks.length; i++) {
+    loadtickets(tasks[i].priority_color, tasks[i].text, tasks[i].id);
+  }
 }
 
 loadTasks();
-
-
